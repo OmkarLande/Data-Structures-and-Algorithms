b5eb18394002df9c5085bffe9c95c8164f98e9a1
KingOm534 <omkarrlande.534@gmail.com>
2023-07-10 22:21:57 +0530

    Basics start

---
+++ Basics/Basic.java
@@ -0,0 +1,38 @@
+package Basics;
+
+import java.util.*;
+
+public class Basic{
+    public static void main(String args[]){
+
+
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+
+        // int a= 10;
+        // int b = 100;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+
+        // byte by=2;
+        // char ch= 'p';
+        // boolean bool = false;
+        // double price =  10.3;
+        // long fol =100000 ;
+        // short n =90;
+        // String og="omkar";
+
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        
+    }
+}
---
+++ Basics/VarQue.java
@@ -0,0 +1,36 @@
+package Basics;
+
+import java.util.*;
+
+public class VarQue {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+
+        //que1 : avg of 3 numbers
+        // System.out.println("Enter 3 numbers");
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+
+        // int avg = (a+b+c)/3;
+
+        // System.out.println(avg);
+
+        //que2 : area of a square
+        // System.out.println("Enter the side of a square");
+        // int side= sc.nextInt();
+        // System.out.println( side*side);
+
+        //que3 : cost of 3 items
+        // System.out.println("Enter the cost of 3 items");
+        // float pen = sc.nextFloat();
+        // float pencil = sc.nextFloat();
+        // float eraser = sc.nextFloat();
+
+        // System.out.println("Total cost will be :" + ((pen+pencil+eraser)-(0.18f *(pen+pencil+eraser))));
+
+
+    }
+}
